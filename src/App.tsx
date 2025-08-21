9f83672a2949c48cec23a70a8f8a884b198b0b9e
utgwkk <utagawakiki@gmail.com>
2025-08-22 00:39:21 +0900

    シェアボタンを置く・タイトルなんとかする・邪魔なCSS消す

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,45 @@ function App() {
   const [unseiIndex, setUnseiIndex] = useState(0);
   useEffect(() => {
     const timer = window.setInterval(() => {
-      setUnseiIndex(curr => (curr + 1) % UNSEIS.length)
+      setUnseiIndex((curr) => (curr + 1) % UNSEIS.length);
     }, 50);
     return () => {
-      window.clearInterval(timer)
-    }
+      window.clearInterval(timer);
+    };
   });
-  return <QRCodeSVG value={UNSEIS[unseiIndex]} />;
+  return (
+    <>
+      <QRCodeSVG value={UNSEIS[unseiIndex]} />
+      <div>
+        <a
+          className="twitter-share-button"
+          href="https://twitter.com/intent/tweet"
+        >
+          Tweet
+        </a>
+      </div>
+      <script async src="https://platform.twitter.com/widgets.js"></script>
+      <a
+        href="https://b.hatena.ne.jp/entry/"
+        className="hatena-bookmark-button"
+        data-hatena-bookmark-layout="vertical-normal"
+        data-hatena-bookmark-lang="ja"
+        title="このエントリーをはてなブックマークに追加"
+      >
+        <img
+          src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png"
+          alt="このエントリーをはてなブックマークに追加"
+          width="20"
+          height="20"
+          style={{border: 'none'}}
+        />
+      </a>
+      <script
+        type="text/javascript"
+        src="https://b.st-hatena.com/js/bookmark_button.js"
+      ></script>
+    </>
+  );
 }
 
 export default App;
