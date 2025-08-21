b33da3eb020e5f40a2e429a93e57b757d6fc6d3a
utgwkk <utagawakiki@gmail.com>
2025-08-22 01:04:49 +0900

    QRコードを読んだらTwitterシェアできるようにする

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import { QRCodeSVG } from "qrcode.react";
 
 const UNSEIS = ["大吉", "中吉", "吉", "末吉", "小吉", "凶", "大凶"] as const;
 
+const generateTwitterShareUrl = (unsei: string) => {
+  const url = new URL(
+    "https://twitter.com/intent/tweet?url=https%3A%2F%2Fsugarheart.utgw.net%2Fqrcode-omikuji%2F"
+  );
+  url.searchParams.set('text', `${unsei}でした!!`)
+  return url;
+};
+
 function App() {
   const [unseiIndex, setUnseiIndex] = useState(0);
   useEffect(() => {
@@ -16,8 +24,9 @@ function App() {
   });
   return (
     <>
-      <QRCodeSVG value={UNSEIS[unseiIndex]} />
-      <div style={{marginTop: '100px'}}>
+      <h1>QRコードを読み込んでTwitterでシェアしてね</h1>
+      <QRCodeSVG value={generateTwitterShareUrl(UNSEIS[unseiIndex]).toString()} />
+      <div style={{ marginTop: "100px" }}>
         <a
           className="twitter-share-button"
           href="https://twitter.com/intent/tweet"
