7523ce871ef3192c209411792012d4b017ecf473
utgwkk <utagawakiki@gmail.com>
2025-08-22 00:43:31 +0900

    ブクマボタン動いていないのを修正したい

--- src/App.tsx
+++ src/App.tsx
@@ -26,25 +26,6 @@ function App() {
         </a>
       </div>
       <script async src="https://platform.twitter.com/widgets.js"></script>
-      <a
-        href="https://b.hatena.ne.jp/entry/"
-        className="hatena-bookmark-button"
-        data-hatena-bookmark-layout="vertical-normal"
-        data-hatena-bookmark-lang="ja"
-        title="このエントリーをはてなブックマークに追加"
-      >
-        <img
-          src="https://b.st-hatena.com/images/v4/public/entry-button/button-only@2x.png"
-          alt="このエントリーをはてなブックマークに追加"
-          width="20"
-          height="20"
-          style={{border: 'none'}}
-        />
-      </a>
-      <script
-        type="text/javascript"
-        src="https://b.st-hatena.com/js/bookmark_button.js"
-      ></script>
     </>
   );
 }
